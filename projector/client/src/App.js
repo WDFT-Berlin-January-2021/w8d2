9e0f5b95fe382b8f9259632970efc6d12b785964
Jan Redmann <janredmann1@gmail.com>
2021-03-02 12:32:21 +0100

    add autentication

--- projector/client/src/App.js
+++ projector/client/src/App.js
@@ -1,23 +1,62 @@
-import { Route } from 'react-router-dom';
+import React from 'react';
 import './App.css';
+import { Route, Redirect } from 'react-router-dom';
 import Projects from './Components/Projects';
 import ProjectDetails from './Components/ProjectDetails';
 import Navbar from './Components/Navbar';
+import Signup from './Components/Signup';
+import Login from './Components/Login';
+import ProtectedRoute from './Components/ProtectedRoute';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <Route
-        exact path='/projects'
-        component={Projects}
-      />
-      <Route
-        exact path='/projects/:id'
-        component={ProjectDetails}
-      />
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    user: this.props.user
+  }
+
+  setUser = user => {
+    this.setState({
+      user: user
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar user={this.state.user} setUser={this.setUser} />
+        <Route
+          exact path='/projects'
+          // component={Projects}
+          render={props => {
+            if (this.state.user) {
+              return <Projects {...props} />
+            } else return <Redirect to='/login' />
+          }}
+        />
+        {/* <ProtectedRoute
+          exact path='/projects'
+          user={this.state.user}
+          component={Projects}
+          redirectPath='/login'
+        /> */}
+        <Route
+          exact path='/projects/:id'
+          component={ProjectDetails}
+        />
+        <Route
+          exact path='/signup'
+          // component={Signup}
+          // if you want to pass props in the routing we use a so called render prop
+          render={props => <Signup setUser={this.setUser} {...props} />}
+        />
+        <Route
+          exact path='/login'
+          render={props => <Login setUser={this.setUser} {...props} />}
+        />
+
+      </div>
+    );
+  }
 }
 
 export default App;
--- projector/client/src/Components/Navbar.js
+++ projector/client/src/Components/Navbar.js
@@ -1,17 +1,42 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { logout } from '../services/auth';
 
-export default function Navbar() {
+const handleLogout = props => {
+  logout().then(() => {
+    props.setUser(null)
+  })
+}
+
+export default function Navbar(props) {
+  console.log(props.user)
   return (
     <div>
       <ul>
         <li>
           <Link to='/'>Home</Link>
         </li>
-        <li>
-          <Link to='/projects'>Projects</Link>
-        </li>
+        {/* If we have a logged in user -> show projects and logout otherwise show login and signup */}
+        {props.user ? (
+          <>
+            <li>
+              <Link to='/projects'>Projects</Link>
+            </li>
+            <li>
+              <Link to='/' onClick={() => handleLogout(props)} >Logout</Link>
+            </li>
+          </>
+        ) : (
+            <>
+              <li>
+                <Link to='/signup'>Signup</Link>
+              </li>
+              <li>
+                <Link to='/login'>Login</Link>
+              </li>
+            </>
+          )}
       </ul>
-    </div>
+    </div >
   )
 }
